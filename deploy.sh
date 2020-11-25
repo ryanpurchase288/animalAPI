fe79f5ae36ef0a55e4d4ad2bc656ccc8a9f90ff8
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-25 15:26:42 +0000

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
-
+sudo apt update
+sudo apt install python3 python3-pip python3-venv
+python3 -m venv venv
+. ./venv/bin/activate
+pip3 install -r requirements.txt
 sudo docker stack deploy --compose-file docker-compose.yaml app
